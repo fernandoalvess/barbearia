64e5e940d295cbccf4dca547d4d6cb577ae00570
fernandoalvess <fernandoumalves@gmail.com>
2026-07-07 15:56:49 -0300

    chore: adicionando next auth setup

--- src/app/_components/sidebar-sheets.tsx
+++ src/app/_components/sidebar-sheets.tsx
@@ -1,10 +1,17 @@
-import { Avatar, AvatarImage } from "./ui/avatar"
-import { HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react"
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
 import { quickSearchOptions } from "../_constants/search"
 import { Button } from "./ui/button"
 import { SheetContent, SheetHeader, SheetTitle, SheetClose } from "./ui/sheet"
 import Image from "next/image"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+  DialogContent,
+} from "./ui/dialog"
 
 const SidebarSheets = () => {
   return (
@@ -13,15 +20,40 @@ const SidebarSheets = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá. Faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando a sua conta
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Google Icon"
+                width={18}
+                height={18}
+              />
+              Entrar com Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://plus.unsplash.com/premium_vector-1714618927767-b7606cc6c88c?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div>
           <p className="font-bold">Fernando Alves</p>
           <p className="text-xs">fernando.alves@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid p-5">
