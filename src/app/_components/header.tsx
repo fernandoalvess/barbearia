a2dd53711b65770688f6ab3e895090b31eb5b338
fernandoalvess <fernandoumalves@gmail.com>
2026-07-08 14:31:53 -0300

    feat: adicionando a busca por titulo e serviço

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,12 +4,15 @@ import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheets from "./sidebar-sheets"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image src="/logo.png" alt="FSW Barber" width={120} height={18} />
+        <Link href="/">
+          <Image src="/logo.png" alt="Click Barber" width={120} height={18} />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
